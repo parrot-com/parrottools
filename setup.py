dded42cd3bdd009e9fae6d0ab0941f7435f4e8f2
Matej Vetrak <matej.vetrak@parrothq.com>
2021-02-19 17:53:16 +0100

    Upgrade struclog and migrate to contextvars python native libraryOD

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
     ],
-    install_requires=["structlog>=20.2.0", "sentry-sdk>=0.19.5"],
+    install_requires=["structlog~=21.1.0", "sentry-sdk~=0.19.5"],
     extras_require={
         "tests": ["pytest>=6.2.1"],
         "dev": ["pytest>=6.2.1", "pre-commit>=2.9.3"],
